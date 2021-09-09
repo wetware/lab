11cc6eec850b576b2d090099a9756569ac07b8e6
Louis Thibault <l.thibault@sentimens.com>
2021-09-09 13:09:13 -0400

    Nuke it from orbit.

--- internal/cmd/app/casm.js
+++
@@ -1,8 +0,0 @@
-export function connect(cfg) {
-    const sock = new WebSocket(cfg.url);
-    sock.onmessage = R.compose(cfg.onstep, R.either(reset, step), payload);
-}
-
-const reset = R.prop("graph"),
-    step = R.prop("step"),
-    payload = R.compose(JSON.parse, R.prop("data"));
