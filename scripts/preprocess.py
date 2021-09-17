71b447471f289ab641a62fe783ebb09cb40cef78
aratz <aratz.m.lasa@gmail.com>
2021-09-17 20:04:38 +0200

    Added preprocessing script and requirements.txt for analyzing convergence

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -14,8 +14,8 @@ from influxdb import InfluxDBClient
               default=100, type=int)
 def preprocess_run(run, output, ticks):
     # Initialize influx client
-    client = InfluxDBClient(host="testground-influxdb", port=8086)
-    client.switch_database("tetsground")
+    client = InfluxDBClient(host="localhost", port=8086)
+    client.switch_database("testground")
 
     # Create csv writer
     if not output:
@@ -24,19 +24,31 @@ def preprocess_run(run, output, ticks):
     writer = csv.writer(f)
 
     peers = {}
-    for point in client.query(
-            'SHOW TAG VALUES from "diagnostics.casm-pex-convergence.view.point" WITH key=peer').get_points():
-        peers[point["peer"]] = 0
-    for tick in range(ticks):
+    peers_seq = {}
+    for i, point in enumerate(client.query(
+            f'''SHOW TAG VALUES from "diagnostics.casm-pex-convergence.view.point" WITH key=peer WHERE run='{run}' ''').get_points()):
+        peers[i] = 0
+        peers_seq[point["value"]] = i
+    for tick in range(1, ticks+1):
         histogram_data = peers.copy()
+        # Extract peer views at tick 'tick'
         results = client.query(f'''SELECT * FROM "diagnostics.casm-pex-convergence.view.point" WHERE run='{run}' ''')
-        for point in results.get_points(tags={"tick": tick}):
+        for point in results.get_points(tags={"tick": str(tick)}):
+            if not point["records"]:
+                continue
             for record in point["records"].split("-")[1:]:
-                peers[record] += 1
-        for key, value in histogram_data.values():
+                histogram_data[peers_seq[record]] += 1
+        for key, value in histogram_data.items():
             writer.writerow([key, value, tick])
     f.close()
 
+    # Write peer->id mapping
+    output = f"{run}.peers"
+    f = open(output, "w")
+    writer = csv.writer(f)
+    for peer, i in peers_seq.items():
+        writer.writerow([i, peer])
+    f.close()
 
 if __name__ == "__main__":
     preprocess_run()
